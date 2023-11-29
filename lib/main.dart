2f2b5022c360f35b6bba5b8dfda934d0021285eb
72467573 <72467573@certus.edu.pe>
2023-11-29 13:18:42 -0500

    Actualizacion de login

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pan a la vista',
-      theme: ThemeData(),
+      theme: ThemeData(
+        primaryColor: Colors.brown,
+        colorScheme: const ColorScheme(
+          brightness: Brightness.light,
+          primary: Color.fromARGB(255, 152, 50, 13),
+          onPrimary: Color.fromARGB(255, 255, 255, 255),
+          secondary: Color.fromARGB(255, 255, 255, 255),
+          onSecondary: Colors.brown,
+          error: Colors.blueGrey,
+          onError: Colors.blueGrey,
+          background: Colors.blueGrey,
+          onBackground: Colors.blueGrey,
+          surface: Colors.blueGrey,
+          onSurface: Colors.blueGrey,
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       home: const MyHomePage(),
     );
--- lib/widgets/boton.dart
+++ lib/widgets/boton.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:panalavista_definitivo/pages/login.dart';
+import 'package:panalavista_definitivo/pages/pestanas_de_logeo/registro1.dart';
 
 class MyBotonInicio extends StatefulWidget {
   const MyBotonInicio({super.key});
@@ -17,7 +18,7 @@ class _MyBotonInicioState extends State<MyBotonInicio> {
           FilledButton(
             onPressed: () {
               Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => MyLoginDeInicio(),
+                builder: (context) => MyRegistroPage(),
               ));
             },
             child: Text(
